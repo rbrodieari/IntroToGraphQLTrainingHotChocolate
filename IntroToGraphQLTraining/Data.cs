3d694d2fe35632aac74572c2220e61d676a3bd81
Rob Brodie <Rob.Brodie@holman.com>
2023-04-12 15:46:14 -0400

    Add project files.

---
+++ IntroToGraphQLTraining/Data.cs
@@ -0,0 +1,53 @@
+﻿namespace IntroToGraphQLTraining
+{
+    public static class Data
+    {
+        public static List<Task> Tasks = new List<Task>
+        {
+            new Task
+            {
+                Id = "rvjmygy9h",
+                Title =  "Learn JavaScript",
+                Complete = false,
+            },
+            new Task
+            {
+                Id = "oszz1w0z9",
+                Title = "Make coffee",
+                Complete = true,
+            },
+            new Task
+            {
+                Id = "e2iliqkfg",
+                Title = "Build a deck",
+                Complete = true,
+            },
+            new Task
+            {
+                Id = "iy93nd667",
+                Title = "Go viral on TikTok",
+                Complete = true,
+            },
+            new Task
+            {
+                Id = "d2d9ymim6",
+                Title = "Solve world hunger",
+                Complete = false,
+            },
+        };
+
+        public static List<User> Users = new List<User>
+        {
+            new User
+            {
+                Id = 123,
+                Name =  "Alice",                
+            },
+            new User
+            {
+                Id = 345,
+                Name = "Bob",                
+            },            
+        };
+    }
+}
---
+++ IntroToGraphQLTraining/Program.cs
@@ -0,0 +1,72 @@
+using IntroToGraphQLTraining;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>()
+    .AddMutationType<Mutation>()
+    .AddMutationConventions(applyToAllMutations: true);
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapGraphQL();
+
+app.Run();
+
+public class Query
+{
+    public int GetTaskCount() => Data.Tasks.Count;
+    public List<Task> GetTasks() => Data.Tasks;
+    public Task? GetTask(string id) => Data.Tasks.Find(task => task.Id == id);
+    public List<User> GetUsers() => Data.Users;
+    public User? GetUser(int id) => Data.Users.Find(user => user.Id == id);
+}
+
+public class Mutation
+{
+    public Task CreateTask(string title)
+    {
+        var newTask = new Task
+        {
+            Id = Convert.ToBase64String(Encoding.UTF8.GetBytes(DateTime.Now.Ticks.ToString())),
+            Title = title,
+            Complete = false
+        };
+
+        Data.Tasks.Add(newTask);
+
+        return newTask;
+    }
+
+    public Task UpdateTask(string id, string title)
+    {       
+        var tempTask = Data.Tasks.Find(task => task.Id == id);
+
+        if (tempTask == null)
+            throw new GraphQLException("Task not found!");
+
+        tempTask.Title = title;
+        return tempTask;
+    }
+
+
+}
+
+public class Task
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public bool Complete { get; set; }
+}
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }    
+}
